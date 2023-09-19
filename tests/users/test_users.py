662ff22787c770a5bcd9827056891a17f6b150ed
YAGolubev <yagolubev@inbox.ru>
2023-09-19 21:57:47 +0300

    PyTest #4.2 | setup, teardown

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -2,10 +2,9 @@ from src.baseclasses.response import Response
 from src.schemas.user import User
 
 
-def test_getting_users_list(get_users, calculate):
+def test_getting_users_list(get_users, make_number):
     Response(get_users).assert_status_code(200).validate(User)
-    print(calculate)
-    print(calculate(1, 1))
+    print(make_number)
 
 
 def test_another():
